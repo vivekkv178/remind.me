248bed0cfb67c9b9af8347e5aab30bed9d5e8bf1
Vivek <vivekkv178@gmail.com>
2024-02-16 07:39:02 +0800

    CRON Update

--- app/triggerReminders/route.js
+++ app/triggerReminders/route.js
@@ -1,5 +1,12 @@
-export const dynamic = "force-dynamic"; // static by default, unless reading the request
+import triggerRemindersService from "./service";
+import { NextResponse } from "next/server";
 
-export function GET(request) {
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+export async function GET(req, res) {
+  try {
+    console.log("Request start");
+    // const response = await triggerRemindersService();
+    return new Response(`Success`);
+  } catch (error) {
+    return new Response(`Fail`);
+  }
 }
