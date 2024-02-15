2d4685803a60d75e4cada7ae69220a46e6ff8fa4
Vivek <vivekkv178@gmail.com>
2024-02-15 19:55:30 +0800

    logs for cron job

--- app/triggerReminders/service.js
+++ app/triggerReminders/service.js
@@ -36,6 +36,7 @@ const triggerRemindersService = () => {
       FIRESTORE_COLLECTIONS.EVENTS
     );
     events.forEach(async (event) => {
+      console.log(event);
       if (checkTrigger(event))
         if (event.contact_id) {
           const contactDetails = await getContactDetails(event);
