72a4e2a1117dee08655f3e20857ffa7228e7c822
Vivek <vivekkv178@gmail.com>
2024-02-16 07:23:25 +0800

    CRON Job update

--- app/triggerReminders/route.js
+++ app/triggerReminders/route.js
@@ -1,11 +1,11 @@
 import triggerRemindersService from "./service";
 import { NextResponse } from "next/server";
 
-export async function POST(req, res) {
+export async function GET(req, res) {
   try {
     console.log("Request start");
-    const response = await triggerRemindersService();
-    return NextResponse.json(response, { status: 200 });
+    // const response = await triggerRemindersService();
+    return NextResponse.json({ response: "" }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { error: "Internal Server Error" },
