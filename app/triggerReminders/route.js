a3da9f2875101e314ebe2bc3bf3097ebc96bb68a
Vivek <vivekkv178@gmail.com>
2024-02-16 06:41:52 +0800

    Cron Job bug fix

--- app/triggerReminders/route.js
+++ app/triggerReminders/route.js
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 
 export async function POST(req, res) {
   try {
-    console.log("Request start");
     const response = await triggerRemindersService();
     return NextResponse.json(response, { status: 200 });
   } catch (error) {
--- app/triggerReminders/service.js
+++ app/triggerReminders/service.js
@@ -36,7 +36,6 @@ const triggerRemindersService = () => {
       FIRESTORE_COLLECTIONS.EVENTS
     );
     events.forEach(async (event) => {
-      console.log(event);
       if (checkTrigger(event))
         if (event.contact_id) {
           const contactDetails = await getContactDetails(event);
