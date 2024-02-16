68bf78d6fb184659a2ae2f97dc7d0b6b227e6597
Vivek <vivekkv178@gmail.com>
2024-02-16 09:41:17 +0800

    Removed PWA

--- app/layout.js
+++ app/layout.js
@@ -13,45 +13,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "remind.me | No Forgetting Anymore",
   description: "Notifications for your Events",
-  "application-name": "remind.me",
-  "apple-mobile-web-app-capable": "yes",
-  "apple-mobile-web-app-status-bar-style": "default",
-  "apple-mobile-web-app-title": "remind.me",
-  description: "No Forgetting Anymore",
-  "format-detection": "telephone=no",
-  "mobile-web-app-capable": "yes",
-  "msapplication-config": "/icon-192x192.png",
-  "msapplication-TileColor": "#2B5797",
-  "msapplication-tap-highlight": "no",
-  "theme-color": "#000000",
-  openGraph: {
-    title: "remind.me",
-    description: "No Forgetting Anymore",
-    url: "https://remind-me-six.vercel.app/",
-    siteName: "remind.me",
-    images: [
-      {
-        url: "/icon-192x192.png", // Must be an absolute URL
-        width: 192,
-        height: 192,
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-  icons: {
-    icon: [
-      { url: "/icon-192x192.png" },
-      { url: "/icon-256x256.png" },
-      { url: "/icon-384x384.png" },
-      { url: "/icon-512x512.png" },
-    ],
-    shortcut: ["/shortcut-icon.png"],
-    apple: [
-      { url: "/icon-192x192.png" },
-      { url: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
-    ],
-  },
 };
 
 const RootLayout = ({ children }) => {
--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa")({
-  dest: "public",
-});
-
 const nextConfig = {
   reactStrictMode: false,
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
